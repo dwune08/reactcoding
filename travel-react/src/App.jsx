20af5f2614b37eb706e509980f10b72c08e7d2ba
dwune08 <dwune159@gmail.com>
2026-08-14 11:09:40 +0900

    여행 기록 프로젝트 중간 커밋

--- travel-react/src/App.jsx
+++ travel-react/src/App.jsx
@@ -1,7 +1,26 @@
+import'./App.css';
+import {Routes, Route, Link} from 'react-router';
+import Home from './pages/Home';
+import New from './pages/New';
+import Travel from './pages/Travel';
+import Edit from './pages/Edit';
+
 const App = () => {
   return (
-    <div>
-      <h1>여행 기록 관리</h1>
+    <div className='App'>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/travel/:id" element={<Travel />} />
+        <Route path="/edit" element={<Edit />} />
+      </Routes>
+
+      <div className='link-wrapper'>
+        <Link to={'/'}>Home</Link>
+        <Link to={'/new'}>새 기록</Link>
+        <Link to={'/travel'}>읽기</Link>
+        <Link to={'/edit'}>수정/삭제</Link>
+      </div>
     </div>
   );
 };
