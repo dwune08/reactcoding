c4c86a3d5a4e658c1bdaf76a65bc1ac521c89f85
dwune08 <dwune159@gmail.com>
2026-08-14 09:57:30 +0900

    일기장 프로젝트 완료

--- project3/src/pages/Edit.jsx
+++ project3/src/pages/Edit.jsx
@@ -3,13 +3,14 @@ import { useParams, useNavigate } from "react-router";
 import useDiary from "../hooks/useDiary";
 import Button from "../components/Button";
 import Header from "../components/Header";
+import Editor from "../components/Editor";
 import { DiaryDispatchContext } from "../context/DiaryContext";
 
 const Edit = () => {
    const { id } = useParams();
    const data = useDiary(id);
    const navigate = useNavigate();
-   const { onDelete } = useContext(DiaryDispatchContext);
+   const { onDelete, onUpdate } = useContext(DiaryDispatchContext);
 
    const goBack = () => {
       navigate(-1);
@@ -19,6 +20,14 @@ const Edit = () => {
       if(window.confirm("일기를 정말 삭제할까요? 다시 복구되지 않아요!")) {
          onDelete(Number(id));
          navigate("/", {replace:true});
+      }
+   };
+
+   const onSubmit = (data) => {
+      if(window.confirm("일기를 정말 수정할까요?")) {
+         const { date, content, emotionId } = data;
+         onUpdate(Number(id), date, content, emotionId);
+         navigate("/", { replace: true});
       }
    };
 
@@ -33,6 +42,8 @@ const Edit = () => {
             leftChild={<Button text="<뒤로 가기" onClick={goBack} />}
             rightChild={<Button text="삭제하기" type="negative" onClick={onClickDelete} />}
          />
+
+         <Editor initData={data} onSubmit={onSubmit}/>
       </div>
    );
 };
